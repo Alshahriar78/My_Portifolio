8dee70528cf53a946be1d97f09678dbf7fbb3f62
Alshahriar78 <alshahriar78@gmail.com>
2024-04-01 02:18:55 +0600

    update experiance part

--- src/Components/experiancepart.jsx
+++ src/Components/experiancepart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const Experiancepart = () => {
     return (
@@ -7,15 +7,18 @@ const Experiancepart = () => {
                 <h2>Experiance</h2>
                 <div id="mern-stack" className="hlaf-width single-experiance">
                     <h2>MERN Stack Web Developer</h2>
-                    <h3 className="name-highlight">2023-Present | Pro-level Developer</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo repudiandae modi cumque officia atque fugit recusandae suscipit dignissimos aperiam ea.</p>
+                    <h3 className="name-highlight">2023-Present | Entry-Level Developer</h3>
+                    <p>I have huge interest in Full Stack Development With MERN . I have the skills in jQuery, Ajax, API testing tool postman. 
+                    I desire to build a professional career as a MERN Stack Developer.</p>
 
                 </div>
                 <div id="baby-web" className="hlaf-width single-experiance">
-                    <h2>Baby Web Developer</h2>
-                    <h3 className="name-highlight">2021-2022 | Pro-level Developer</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo repudiandae modi cumque officia atque fugit recusandae suscipit dignissimos aperiam ea.</p>
+                    <h2> Web Developer</h2>
+                    <h3 className="name-highlight">2021-2022 | Beginner-Level Developer</h3>
+                    <p>I’m a self-motivated, hard-working and passionate person looking for an opportunity to use my talent, academic skills, and technical knowledge to state my ability to enhance my educational qualifications.
+                    I have several programming language skills (C, C++, HTML, CSS,  JavaScript)  </p>
                 </div>
+                
             </section>
         </div>
     );
